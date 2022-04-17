f1fc7b07177c2d49c6c87e9e14adb83f2c960b28
Puppy Spike <albrechtboucherweb@gmail.com>
2022-04-17 00:53:26 +0000

    build: Neue Dateien damit das Plugin funktioniert

---
+++ includes/class-spikes-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://felixlimburger.de
+ * @since      1.0.0
+ *
+ * @package    Spikes_Plugin
+ * @subpackage Spikes_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Spikes_Plugin
+ * @subpackage Spikes_Plugin/includes
+ * @author     Felix Limburger <info@felixlimburger.de>
+ */
+class Spikes_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-spikes-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://felixlimburger.de
+ * @since      1.0.0
+ *
+ * @package    Spikes_Plugin
+ * @subpackage Spikes_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Spikes_Plugin
+ * @subpackage Spikes_Plugin/includes
+ * @author     Felix Limburger <info@felixlimburger.de>
+ */
+class Spikes_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
